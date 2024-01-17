ed6b131e55adf84c942552db8c382286457a28f3
eni <129673786+enitimeago@users.noreply.github.com>
2024-01-17 23:11:06 +0900

    Add NDMMD_VRCSDK3_AVATARS ifdef
    This is similar to https://github.com/bdunderscore/modular-avatar/pull/504 and https://github.com/anatawa12/AvatarOptimizer/pull/609 which are motivated by supporting non-VRChat avatars. There are no immediate plans on my end to add non-VRChat avatar support yet, but I intend to pull in code from these repositories where appropriate, and this would make syncing with upstream easier.

--- Packages/enitimeago.non-destructive-mmd/Editor/MapperPlugin.cs
+++ Packages/enitimeago.non-destructive-mmd/Editor/MapperPlugin.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿#if NDMMD_VRCSDK3_AVATARS
+
+using System.Collections.Generic;
 using System.Linq;
 using nadena.dev.ndmf;
 using nadena.dev.ndmf.fluent;
@@ -88,3 +90,5 @@ namespace enitimeago.NonDestructiveMMD
         }
     }
 }
+
+#endif
