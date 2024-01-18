2fad73e202c3da750097608357c8a8085e473fa5
eni <129673786+enitimeago@users.noreply.github.com>
2024-01-19 00:17:45 +0900

    Add share as .unitypackage functionality
    Fixes #6

--- Packages/enitimeago.non-destructive-mmd/Editor/MappingsEditor.cs
+++ Packages/enitimeago.non-destructive-mmd/Editor/MappingsEditor.cs
@@ -18,10 +18,64 @@ namespace enitimeago.NonDestructiveMMD
 
             var data = (BlendShapeMappings)target;
 
+            EditorGUILayout.BeginHorizontal();
+
             if (GUILayout.Button("Open Editor"))
             {
                 MappingsEditorWindow.ShowWindow(data);
             }
+
+            // Extras menu
+            if (GUILayout.Button("▼", EditorStyles.miniButton, GUILayout.Width(20)))
+            {
+                var menu = new GenericMenu();
+                menu.AddItem(new GUIContent("Share as .unitypackage..."), false, () => ExportPackage(data));
+                menu.ShowAsContext();
+            }
+
+            EditorGUILayout.EndHorizontal();
+        }
+
+        private void ExportPackage(BlendShapeMappings mappingsComponent)
+        {
+            string packagePath = EditorUtility.SaveFilePanel(
+                "Save .unitypackage",
+                "",
+                "",
+                "unitypackage");
+
+            if (string.IsNullOrEmpty(packagePath))
+            {
+                return;
+            }
+            
+            // Create a new GameObject with a clone of current mappings.
+            // The name doesn't matter because it will match the prefab's name.
+            // TODO: implement a copyinto method, or see if it's possible to clone the BlendShapeMappings object and add it into the new GameObject.
+            var newGameObject = new GameObject();
+            var newMappingsComponent = newGameObject.AddComponent<BlendShapeMappings>();
+            newMappingsComponent.blendShapeMappings.AddRange(mappingsComponent.blendShapeMappings);
+
+            // Ensure a unique path for the prefab.
+            // TODO: choose a temp folder so that this doesn't turn into "MakeItMMD 1" etc. if it already exists?
+            string prefabPath = AssetDatabase.GenerateUniqueAssetPath("Assets/MakeItMMD.prefab");
+
+            // Save the prefab.
+            bool prefabSuccess;
+            PrefabUtility.SaveAsPrefabAsset(newGameObject, prefabPath, out prefabSuccess);
+            if (!prefabSuccess)
+            {
+                // TODO: see if this can be presented as a dialog.
+                Debug.Log("Prefab failed to save");
+                return;
+            }
+
+            // Export the prefab as a package.
+            AssetDatabase.ExportPackage(prefabPath, packagePath);
+
+            // Delete the prefab.
+            // This is safe because AssetDatabase.GenerateUniqueAssetPath was used.
+            AssetDatabase.DeleteAsset(prefabPath);
         }
     }
 }
