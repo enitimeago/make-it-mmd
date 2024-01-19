fc8c18c42fddc60255befbd50c2ec9643feaa51f
eni <129673786+enitimeago@users.noreply.github.com>
2024-01-19 23:39:45 +0900

    Refactor MMD blend shapes into a separate class
    And decouple dependency on knowing how the MMD blend shapes are stored. We want to reuse the MMD blend shape list in the inspector.

--- Packages/enitimeago.non-destructive-mmd/Editor/MappingsEditorWindow.cs
+++ Packages/enitimeago.non-destructive-mmd/Editor/MappingsEditorWindow.cs
@@ -32,17 +32,17 @@ namespace enitimeago.NonDestructiveMMD
         {
             var window = GetWindow<MappingsEditorWindow>("Make It MMD");
             window._dataSource = data;
+
+            // Retrieve blend shape settings that match known MMD keys.
             foreach (var mapping in data.blendShapeMappings)
             {
-                // Every 3rd item is Japanese, for now only care about Japanese MMD keys.
-                int index = MmdBlendShapeNames
-                    .Select((name, i) => name == mapping.mmdKey ? i : -1)
-                    .Where(i => i >= 0 && (i + 1) % 3 == 0)
-                    .DefaultIfEmpty(-1)
-                    .First();
-                if (index >= 0)
+                foreach ((string knownMmdKey, int i) in MMDBlendShapes.JapaneseNames())
                 {
-                    window._knownBlendShapeMappings.Add(index, mapping.avatarKey);
+                    if (mapping.mmdKey == knownMmdKey)
+                    {
+                        window._knownBlendShapeMappings.Add(i, mapping.avatarKey);
+                        break;
+                    }
                 }
             }
         }
@@ -94,7 +94,7 @@ namespace enitimeago.NonDestructiveMMD
 
             _leftPaneScroll = GUILayout.BeginScrollView(_leftPaneScroll);
 
-            for (int i = 2; i < MmdBlendShapeNames.Length; i += 3)
+            foreach ((string name, int i) in MMDBlendShapes.JapaneseNames())
             {
                 var buttonStyle = _knownBlendShapeMappings.ContainsKey(i) ? _hasValueStyle : _defaultStyle;
                 if (i == _currentMmdKeyIndex)
@@ -102,7 +102,7 @@ namespace enitimeago.NonDestructiveMMD
                     buttonStyle = _knownBlendShapeMappings.ContainsKey(i) ? _selectedHasValueStyle : _selectedStyle;
                 }
 
-                if (GUILayout.Button(MmdBlendShapeNames[i], buttonStyle))
+                if (GUILayout.Button(name, buttonStyle))
                 {
                     _currentMmdKeyIndex = i;
                 }
@@ -121,7 +121,7 @@ namespace enitimeago.NonDestructiveMMD
 
             if (_currentMmdKeyIndex >= 0 && _faceBlendShapes != null)
             {
-                GUILayout.Label("Select blendshape for " + MmdBlendShapeNames[_currentMmdKeyIndex]);
+                GUILayout.Label("Select blendshape for " + MMDBlendShapes.Names[_currentMmdKeyIndex]);
 
                 string selectedBlendShape;
                 _knownBlendShapeMappings.TryGetValue(_currentMmdKeyIndex, out selectedBlendShape);
@@ -130,7 +130,7 @@ namespace enitimeago.NonDestructiveMMD
                 {
                     Debug.Log("Unselected blendshape");
                     _knownBlendShapeMappings.Remove(_currentMmdKeyIndex);
-                    _dataSource.RemoveBlendShapeMapping(MmdBlendShapeNames[_currentMmdKeyIndex]);
+                    _dataSource.RemoveBlendShapeMapping(MMDBlendShapes.Names[_currentMmdKeyIndex]);
                 }
 
                 foreach (var blendShapeName in _faceBlendShapes)
@@ -139,7 +139,7 @@ namespace enitimeago.NonDestructiveMMD
                     {
                         Debug.Log("Selected blendshape: " + blendShapeName);
                         _knownBlendShapeMappings[_currentMmdKeyIndex] = blendShapeName;
-                        _dataSource.SetBlendShapeMapping(MmdBlendShapeNames[_currentMmdKeyIndex], blendShapeName);
+                        _dataSource.SetBlendShapeMapping(MMDBlendShapes.Names[_currentMmdKeyIndex], blendShapeName);
                     }
                 }
             }
@@ -165,102 +165,5 @@ namespace enitimeago.NonDestructiveMMD
             backgroundTexture.Apply();
             return backgroundTexture;
         }
-
-        // https://github.com/anatawa12/AvatarOptimizer/blob/083142dc37538d6812f0a5d9a49b4e278c47c164/Editor/AnimatorParsers/AnimatorParser.cs#L600
-        private static readonly string[] MmdBlendShapeNames = new[] {
-            // New EN by Yi MMD World
-            //  https://docs.google.com/spreadsheets/d/1mfE8s48pUfjP_rBIPN90_nNkAIBUNcqwIxAdVzPBJ-Q/edit?usp=sharing
-            // Old EN by Xoriu
-            //  https://booth.pm/ja/items/3341221
-            //  https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/0b7b5e4b-c62e-41f7-8ced-1f3e58c4f5bf/d5nbmvp-5779f5ac-d476-426c-8ee6-2111eff8e76c.png
-            // Old EN, New EN, JA,
-
-            // ===== Mouth =====
-            "a",            "Ah",               "あ",
-            "i",            "Ch",               "い",
-            "u",            "U",                "う",
-            "e",            "E",                "え",
-            "o",            "Oh",               "お",
-            "Niyari",       "Grin",             "にやり",
-            "Mouse_2",      "∧",                "∧",
-            "Wa",           "Wa",               "ワ",
-            "Omega",        "ω",                "ω",
-            "Mouse_1",      "▲",                "▲",
-            "MouseUP",      "Mouth Horn Raise", "口角上げ",
-            "MouseDW",      "Mouth Horn Lower", "口角下げ",
-            "MouseWD",      "Mouth Side Widen", "口横広げ",
-            "n",            null,               "ん",
-            "Niyari2",      null,               "にやり２",
-            // by Xoriu only
-            "a 2",          null,               "あ２",
-            "□",            null,               "□",
-            "ω□",           null,               "ω□",
-            "Smile",        null,               "にっこり",
-            "Pero",         null,               "ぺろっ",
-            "Bero-tehe",    null,               "てへぺろ",
-            "Bero-tehe2",   null,               "てへぺろ２",
-
-            // ===== Eyes =====
-            "Blink",        "Blink",            "まばたき",
-            "Smile",        "Blink Happy",      "笑い",
-            "> <",          "Close><",          "はぅ",
-            "EyeSmall",     "Pupil",            "瞳小",
-            "Wink-c",       "Wink 2 Right",     "ｳｨﾝｸ２右",
-            "Wink-b",       "Wink 2",           "ウィンク２",
-            "Wink",         "Wink",             "ウィンク",
-            "Wink-a",       "Wink Right",       "ウィンク右",
-            "Howawa",       "Calm",             "なごみ",
-            "Jito-eye",     "Stare",            "じと目",
-            "Ha!!!",        "Surprised",        "びっくり",
-            "Kiri-eye",     "Slant",            "ｷﾘｯ",
-            "EyeHeart",     "Heart",            "はぁと",
-            "EyeStar",      "Star Eye",         "星目",
-            "EyeFunky",     null,               "恐ろしい子！",
-            // by Xoriu only
-            "O O",          null,               "はちゅ目",
-            "EyeSmall-v",   null,               "瞳縦潰れ",
-            "EyeUnderli",   null,               "光下",
-            "EyHi-Off",     null,               "ハイライト消",
-            "EyeRef-off",   null,               "映り込み消",
-
-            // ===== Eyebrow =====
-            "Smily",        "Cheerful",         "にこり",
-            "Up",           "Upper",            "上",
-            "Down",         "Lower",            "下",
-            "Serious",      "Serious",          "真面目",
-            "Trouble",      "Sadness",          "困る",
-            "Get angry",    "Anger",            "怒り",
-            null,           "Front",            "前",
-            
-            // ===== Eyes + Eyebrow Feeling =====
-            // by Xoriu only
-            "Joy",          null,               "喜び",
-            "Wao!?",        null,               "わぉ!?",
-            "Howawa ω",     null,               "なごみω",
-            "Wail",         null,               "悲しむ",
-            "Hostility",    null,               "敵意",
-
-            // ===== Other ======
-            null,           "Blush",            "照れ",
-            "ToothAnon",    null,               "歯無し下",
-            "ToothBnon",    null,               "歯無し上",
-            null,           null,               "涙",
-
-            // others
-
-            // https://gist.github.com/lilxyzw/80608d9b16bf3458c61dec6b090805c5
-            null, null, "しいたけ",
-
-            // https://site.nicovideo.jp/ch/userblomaga_thanks/archive/ar1471249
-            null, null, "なぬ！",
-            null, null, "はんっ！",
-            null, null, "えー",
-            null, null, "睨み",
-            null, null, "睨む",
-            null, null, "白目",
-            null, null, "瞳大",
-            null, null, "頬染め",
-            null, null, "青ざめ",
-        };
     }
 }
