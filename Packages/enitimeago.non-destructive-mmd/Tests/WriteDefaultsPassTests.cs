4e951be10c3467f7b7844c6a06ec177480087f58
eni <129673786+enitimeago@users.noreply.github.com>
2024-02-04 18:02:46 +0900

    Assert write defaults pass sets write defaults on

--- Packages/enitimeago.non-destructive-mmd/Tests/WriteDefaultsPassTests.cs
+++ Packages/enitimeago.non-destructive-mmd/Tests/WriteDefaultsPassTests.cs
@@ -1,19 +1,29 @@
+using System.Linq;
 using enitimeago.NonDestructiveMMD;
 using enitimeago.NonDestructiveMMD.vendor;
 using nadena.dev.ndmf;
 using NUnit.Framework;
+using UnityEditor.Animations;
+using VRC.SDK3.Avatars.Components;
 
 public class WriteDefaultsPassTests : TestBase
 {
-    // TODO: This is just a smoke test for now.
     [Test]
-    public void RunPass_WithBasicAvatar()
+    public void RunPass_WithBasicAvatar_SetsWriteDefaultOn()
     {
         var pass = new WriteDefaultsPass();
         var avatar = CreateAvatarWithExpectedFaceNameAndFX();
+        var descriptor = avatar.GetComponent<VRCAvatarDescriptor>();
         var buildContext = new BuildContext(avatar, null);
         AnimationUtil.CloneAllControllers(buildContext);
 
         pass.Execute(avatar);
+
+        var fxController = descriptor.baseAnimationLayers
+            .First(layer => layer.type == VRCAvatarDescriptor.AnimLayerType.FX)
+            .animatorController as AnimatorController;
+        fxController.layers.ToList().ForEach(
+            layer => layer.stateMachine.states.ToList().ForEach(
+                state => Assert.IsTrue(state.state.writeDefaultValues)));
     }
 }
