enitimeago/make-it-mmd

commit 27b0b558f44ee4bada5e6751577bb605d89c9a01

Simplify Localization class
Also move LocalizationAsset init to happen inside NDMF assetLoader callback rather than at static constructor time

Packages/enitimeago.non-destructive-mmd/Editor/Localization.cs
@@ -1,58 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.Linq;
 using nadena.dev.ndmf.localization;
 using UnityEditor;
 using UnityEngine;
 
 namespace enitimeago.NonDestructiveMMD
 {
-    [InitializeOnLoad]
     internal static class Localization
     {
-        private static List<LocalizationAsset> _localizationAssets;
-        private static string[] _localeCodes;
-        private static string[] _localeNames;
-        private static int _currentLocaleIndex;
+        private class Locale
+        {
+            public string IsoCode;
+            public LocalizationAsset Asset;
+        }
+
         public static Localizer Localizer { get; private set; }
+        private static Locale[] _locales = new Locale[]
+        {
+            new Locale { IsoCode = "en-us" },
+            new Locale { IsoCode = "ja-jp" },
+        };
+        private static int _currentLocaleIndex = 0;
 
         static Localization()
         {
-            string localizationDir = AssetDatabase.GUIDToAssetPath("85d7850cb8f79884ab5fe8d20e382df5");
-            string[] localizationDirFiles = Directory.GetFiles(localizationDir);
-            if (localizationDirFiles.Length == 0)
-            {
-                Debug.LogError($"Make It MMD translation files not found! Tried searching {localizationDir}");
-                return;
-            }
-            _localizationAssets = localizationDirFiles
-                .Select(AssetDatabase.LoadAssetAtPath<LocalizationAsset>)
+            Localizer = new Localizer("en-us", () =>
+                _locales.Select(locale =>
+                {
+                    if (locale.Asset == null)
+                    {
+                        string localizationDir = AssetDatabase.GUIDToAssetPath("85d7850cb8f79884ab5fe8d20e382df5");
+                        locale.Asset = AssetDatabase.LoadAssetAtPath<LocalizationAsset>($"{localizationDir}/{locale.IsoCode}.po");
+                        if (locale.Asset != null)
+                        {
+                            Debug.Log($"Make It MMD successfully loaded LocalizationAsset for {locale.IsoCode}");
+                        }
+                        else
+                        {
+                            Debug.LogError($"Make It MMD failed to load LocalizationAsset for {locale.IsoCode}");
+                        }
+                    }
+                    return locale.Asset;
+                })
                 .Where(asset => asset != null)
-                .OrderBy(asset => asset.localeIsoCode)
-                .ToList();
-            if (_localizationAssets.Count == 0)
-            {
-                Debug.LogError("Make It MMD translation files failed to be loaded!");
-                return;
-            }
-            _localeCodes = _localizationAssets
-                .Select(asset => asset.localeIsoCode.ToLower())
-                .ToArray();
-            _localeNames = _localizationAssets
-                .Select(asset => asset.GetLocalizedString($"locale:{asset.localeIsoCode}"))
-                .ToArray();
-            Localizer = new Localizer("en-US", () => _localizationAssets);
-            OnLanguageChange();
+                .ToList());
             LanguagePrefs.RegisterLanguageChangeCallback(typeof(Localization), _ => OnLanguageChange());
         }
 
         public static void DrawLanguagePicker()
         {
-            int newLocaleIndex = EditorGUILayout.Popup(_currentLocaleIndex, _localeNames);
+            int newLocaleIndex = EditorGUILayout.Popup(
+                _currentLocaleIndex,
+                _locales
+                    .Select(locale => locale.Asset?.GetLocalizedString($"locale:{locale.IsoCode}") ?? locale.IsoCode)
+                    .ToArray());
             if (newLocaleIndex != _currentLocaleIndex)
             {
-                LanguagePrefs.Language = _localeCodes[newLocaleIndex];
+                LanguagePrefs.Language = _locales[newLocaleIndex].IsoCode;
                 _currentLocaleIndex = newLocaleIndex;
             }
         }
@@ -68,14 +71,12 @@ namespace enitimeago.NonDestructiveMMD
             {
                 return value;
             }
-            // Note this will silently fallback, it would be too noisy to log on every message fetch failure.
-            // Instead rely on debug messages on init being caught.
             return fallback;
         }
 
         private static void OnLanguageChange()
         {
-            _currentLocaleIndex = Array.IndexOf(_localeCodes, LanguagePrefs.Language);
+            _currentLocaleIndex = _locales.TakeWhile(locale => locale.IsoCode != LanguagePrefs.Language).Count();
         }
     }
 }

Packages/enitimeago.non-destructive-mmd/Editor/Localization/en-us.po
@@ -1,8 +1,8 @@
 msgid ""
 msgstr ""
-"Language: en-US\n"
+"Language: en-us\n"
 
-msgid "locale:en-US"
+msgid "locale:en-us"
 msgstr "English"
 
 msgid "CommonChecks:NoMMDComponents"

Packages/enitimeago.non-destructive-mmd/Editor/Localization/ja-jp.po
@@ -1,8 +1,8 @@
 msgid ""
 msgstr ""
-"Language: ja-JP\n"
+"Language: ja-jp\n"
 
-msgid "locale:ja-JP"
+msgid "locale:ja-jp"
 msgstr "日本語"
 
 msgid "CommonChecks:NoMMDComponents"

commit 1e36bac2360c0c562575ac13b99227e0564441ca

Fix language picker wrong display regression
The refactor in 27b0b558f44ee4bada5e6751577bb605d89c9a01 removed the initial call to check what language is currently selected.

Packages/enitimeago.non-destructive-mmd/Editor/Localization.cs
@@ -44,6 +44,7 @@ namespace enitimeago.NonDestructiveMMD
                 .Where(asset => asset != null)
                 .ToList());
             LanguagePrefs.RegisterLanguageChangeCallback(typeof(Localization), _ => OnLanguageChange());
+            OnLanguageChange();
         }
 
         public static void DrawLanguagePicker()
@@ -76,7 +77,8 @@ namespace enitimeago.NonDestructiveMMD
 
         private static void OnLanguageChange()
         {
-            _currentLocaleIndex = _locales.TakeWhile(locale => locale.IsoCode != LanguagePrefs.Language).Count();
+            _currentLocaleIndex = _locales.TakeWhile(locale => locale.IsoCode != LanguagePrefs.Language.ToLower()).Count();
+            Debug.Log($"currentLocale {_currentLocaleIndex}");
         }
     }
 }