cafbe7e3031d8452773f83c54a2f6131839e489f
eni <129673786+enitimeago@users.noreply.github.com>
2024-03-20 00:31:54 +0900

    Initial BlendShapeMappings unit test for migration

---
+++ Packages/enitimeago.non-destructive-mmd/Tests/BlendShapeMappingsTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using enitimeago.NonDestructiveMMD;
+using NUnit.Framework;
+using UnityEngine;
+
+public class BlendShapeMappingsTests
+{
+    [Test]
+    public void OnValidate_MigratesDataVersion0()
+    {
+        var blendShapeMappings = new GameObject().AddComponent<BlendShapeMappings>();
+        blendShapeMappings.dataVersion = 0;
+        blendShapeMappings.blendShapeMappings.Add(new MMDToAvatarBlendShape("MmdKey1", new string[] { }) { legacyAvatarKey = "LegacyAvatarKey1" });
+        blendShapeMappings.blendShapeMappings.Add(new MMDToAvatarBlendShape("MmdKey2", new string[] { }) { legacyAvatarKey = "LegacyAvatarKey2" });
+
+        blendShapeMappings.OnValidate();
+
+        Assert.AreEqual(1, blendShapeMappings.dataVersion);
+        Assert.AreEqual(2, blendShapeMappings.blendShapeMappings.Count);
+        var mmdKey1Mapping = blendShapeMappings.blendShapeMappings.FirstOrDefault(x => x.mmdKey == "MmdKey1");
+        var mmdKey2Mapping = blendShapeMappings.blendShapeMappings.FirstOrDefault(x => x.mmdKey == "MmdKey2");
+        Assert.IsNotNull(mmdKey1Mapping);
+        Assert.IsNotNull(mmdKey2Mapping);
+        Assert.AreEqual(new string[] { "LegacyAvatarKey1" }, mmdKey1Mapping.avatarKeys);
+        Assert.AreEqual(new string[] { "LegacyAvatarKey2" }, mmdKey2Mapping.avatarKeys);
+    }
+}
